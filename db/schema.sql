de338ae486ce083d1e47f569e71827698ce383ff
Zino Ebiala <eebia002@fiu.edu>
2023-06-28 15:52:14 -0400

    Just added tables and db into the schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,24 @@
 DROP DATABASE IF EXISTS deparments_db;
 CREATE DATABASE deparments_db;
+
+USE deparments_db;
+
+CREATE TABLE department{
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  names VARCHAR(30) NOT NULL
+}
+
+CREATE TABLE role{
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT
+}
+
+CREATE TABLE employee{
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+}
