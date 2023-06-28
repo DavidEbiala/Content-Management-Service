8be7c810597936b7e59253a5d1dd48dfe55fdbbf
Zino Ebiala <eebia002@fiu.edu>
2023-06-28 16:23:33 -0400

    Structured the tables for seeds.sql, need to add queries to join them together

--- db/schema.sql
+++ db/schema.sql
@@ -8,15 +8,15 @@ CREATE TABLE department{
   names VARCHAR(30) NOT NULL
 }
 
-CREATE TABLE role{
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE roles{
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT
 }
 
 CREATE TABLE employee{
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (names)
+VALUES ("Engineering"),
+        ("Finance"),
+        ("Legal"),
+        ("Sales");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Lead",100000,),
+("Salesperson", 80000, ),
+("Lead Engineer", 150000, ),
+("Software Engineer", 120000, ),
+("Accountant Manager", 160000, ),
+("Accountant", 125000, ),
+("Legal Team Lead", 250000, ),
+("Lawyer", 190000, );
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", , NULL),
+("Mike", "Chan", , ),
+("Ashley", "Rodriguez", , NULL),
+("Kevin", "Tupik", , ),
+("Kunal", "Singh", , NULL),
+("Malia", "Brown", , ),
+("Sarah", "Lourd", , NULL),
+("Tom", "Allen", , );
