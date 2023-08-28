ec0efa132186557b078caca9baaf20b8b3223262
Zino Ebiala <eebia002@fiu.edu>
2023-08-28 14:26:34 -0400

    disregarding and deleting the connection.js, will just focus and put everything on script.js

--- script/script.js
+++ script/script.js
@@ -1,8 +1,26 @@
+const express = require('express');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const connection = require('../config/connection.js')
 
 
-inquirer
+connection.connect(function (err) {
+  if(err) throw(err)
+  console.log("connected as id " + connection.threadId);
+    console.log(`
+    ╔═══╗─────╔╗──────────────╔═╗╔═╗
+    ║╔══╝─────║║──────────────║║╚╝║║
+    ║╚══╦╗╔╦══╣║╔══╦╗─╔╦══╦══╗║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
+    ║╔══╣╚╝║╔╗║║║╔╗║║─║║║═╣║═╣║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
+    ║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
+    ╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
+    ───────║║──────╔═╝║─────────────────────╔═╝║
+    ───────╚╝──────╚══╝─────────────────────╚══╝`)
+    // runs the app
+    firstPrompt();
+});
+function firstPrompt(){
+  inquirer
   .prompt([
     {
       type: 'list',
@@ -35,12 +53,26 @@ inquirer
         updateEmployee();
     }
   });
+}
 
-  const viewAllDepartments = () =>{
-    const query = `SELECT department.id AS id, department.names AS department FROM department`;
-    connection.promise().query(query, (error, response) => {
-        if (error) throw error;
-        console.table(response);
-        promptUser();
-      });
+
+  function viewAllEmployees () {
+    console.log("Viewing employees\n");
+    const query = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+    ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+    ON m.id = e.manager_id`
+    
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      console.table(res);
+      console.log("Employees viewed!\n");
+  
+      firstPrompt();
+    });
   }
